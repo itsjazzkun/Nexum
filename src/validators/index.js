4decdf941c4232e62aa1606ad00fcbc4dd316398
itsjazzkun <vanced.exe@gmail.com>
2026-08-12 04:08:49 +0530

    updated with custom validators

---
+++ src/validators/index.js
@@ -0,0 +1,38 @@
+import { body } from "express-validator";
+const userRegisterValidator = () => {
+
+    return[
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invaid please enter a vaild email"),
+    body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("username is required")
+        .isLowercase()
+        .withMessage("username must be in lower case")
+        .isLength({min:3})
+        .withMessage("username must atleast 3 characters long"),
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required"),
+    body("Full Name")
+    .optional()
+    .trim()
+
+    ];
+}
+
+
+
+
+
+
+
+export{
+    userRegisterValidator
+}
