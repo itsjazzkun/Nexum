bfc7e0a17a75f1a8db0a889e1d0591a68b686c33
itsjazzkun <vanced.exe@gmail.com>
2026-08-12 04:32:37 +0530

    updated login validators

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -70,4 +70,47 @@ const registerUser = asyncHandler(async (req, res )=>{
   )
 });
 
-export { registerUser };
+const login = asyncHandler(async (req,res)=> {
+    const {email,password,username} = req.body
+    if (!email) {
+        throw new ApiError(400, "email is required")
+    }
+    const user = await User.findOne({email});
+    if (!user) {
+      throw new ApiError(400 , "User does not exist");
+    }
+    const isPasswordVaild = await user.isPasswordVaild
+    (password);
+    if (!isPasswordVaild) {
+         throw new ApiError(400, "Invaid credentials");
+    }
+    const{accessToken, refreshToken} = await
+     generateAccessandRefreshToken(user._id)
+
+     const loggedinUser = await User.findById(user._id).select(
+       "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+     );
+     const options ={
+        httpOnly:true,
+        secure:true,
+     }
+     return res
+     .status(200)
+     .cookie("accessToken",accessToken, options)
+     .cookie("refreshToken",refreshToken, options)
+     .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedinUser,
+                accessToken,
+                refreshToken
+            },
+            "User logged in successfully"
+        )
+     )
+
+});
+
+
+export { registerUser,login };
